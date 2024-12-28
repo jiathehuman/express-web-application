e3e0d2dd70424565e6874e6678aa95962a09e396
jiathehuman <jiathehuman@gmail.com>
2024-12-28 16:39:54 +0800

    Version uploaded on Coursera

--- Midterms/Database_midterms_app/index.js
+++ Midterms/Database_midterms_app/index.js
@@ -9,7 +9,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // MySQL used for the database in this project
 const mysql = require("mysql2");
- // DotEnv module loads environment from .env into process .env, reading key-value pairs and seeting them as env vars
+// DotEnv module loads environment from .env into process .env, reading key-value pairs and seeting them as env vars
 const env = require("dotenv").config();
 
 const app = express(); // instantiate Express
@@ -35,21 +35,21 @@ app.get("/", (req, res) => {
 
 // Get all sports
 app.get("/sports", (req, res) => {
-    const sql = `SELECT * FROM sports`;
-    db.query(sql, function (err, result) {
-      if (err) throw err;
-      res.render("sports", {
-        data: result,
-      });
+  const sql = `SELECT * FROM sports`;
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render("sports", {
+      data: result,
     });
   });
+});
 
 // Get all the events that the athlete had participated in
 // Reference for using Case with Order By: https://stackoverflow.com/questions/19486882/case-when-statement-for-order-by-clause
 app.get("/athlete", (req, res) => {
-    let id = req.query.athlete_id;
-    // Order by age so that the earlier games appear first, then depending on the medal
-    const sql = `SELECT a.name, ag.age, ag.height, ag.weight, n.team, g.season, g.city, g.year, aem.medal, e.event_name
+  let id = req.query.athlete_id;
+  // Order by age so that the earlier games appear first, then depending on the medal
+  const sql = `SELECT a.name, ag.age, ag.height, ag.weight, n.team, g.season, g.city, g.year, aem.medal, e.event_name
       FROM athlete_game ag
       INNER JOIN noc n ON n.noc_id = ag.noc_id
       INNER JOIN athletes a ON ag.athlete_id = a.athlete_id
@@ -66,13 +66,13 @@ app.get("/athlete", (req, res) => {
       END,
       medal
       `;
-    db.query(sql, [id], (err, result) => {
-      if (err) throw err;
-      res.render("athlete", {
-        data: result,
-      });
+  db.query(sql, [id], (err, result) => {
+    if (err) throw err;
+    res.render("athlete", {
+      data: result,
     });
   });
+});
 
 // Question One: How many sports were there at each game?
 app.get("/games_no", (req, res) => {
@@ -127,7 +127,7 @@ app.get("/oldest_olympian", (req, res) => {
 // Question Four: Which NOC won the most number of Gold, Silver and Bronze medals?
 // Reference for SUM and CASE: https://learnsql.com/blog/case-when-with-sum/
 app.get("/noc_games", (req, res) => {
-    const sql = `SELECT n.noc, g.city, g.year, g.season,
+  const sql = `SELECT n.noc, g.city, g.year, g.season,
          SUM(CASE WHEN aem.medal = 'Gold' THEN 1 ELSE 0 END) AS gold,
          SUM(CASE WHEN aem.medal = 'Silver' THEN 1 ELSE 0 END) AS silver,
          SUM(CASE WHEN aem.medal = 'Bronze' THEN 1 ELSE 0 END) AS bronze,
@@ -138,13 +138,13 @@ app.get("/noc_games", (req, res) => {
          LEFT JOIN athlete_event_medal aem ON ag.athlete_id = aem.athlete_id
          GROUP BY n.noc, g.game_id, g.year, g.season
          ORDER BY total DESC`;
-    db.query(sql, function (err, result) {
-      if (err) throw err;
-      res.render("noc_games", {
-        data: result,
-      });
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render("noc_games", {
+      data: result,
     });
   });
+});
 
 // Question Five: From all four games, which 10 female athletes accoumulated the most gold medals?
 app.get("/female_most_medals", (req, res) => {
@@ -214,7 +214,7 @@ app.get("/summer_winter", (req, res) => {
     GROUP BY a.athlete_id, a.name, a.sex
     HAVING COUNT(DISTINCT g.season) = 2)
 
-    SELECT p.name, p.sex, e.event_name, g.season, g.city, g.year, aem.medal
+    SELECT p.name, p.sex, e.event_name, g.season, g.city, g.year, aem.medal, p.athlete_id
     FROM athlete_event_medal aem
     RIGHT JOIN pro_athletes p
     ON aem.athlete_id = p.athlete_id
@@ -234,43 +234,69 @@ app.get("/summer_winter", (req, res) => {
 // Question Nine: Are there athletes that won Bronze in London 2012 and won a Gold medal at Rio 2016?
 // Reference for existance of rows: https://dba.stackexchange.com/questions/159413/exists-select-1-vs-exists-select-one-or-the-other
 // Reference for EXISTS clause: https://www.w3schools.com/sql/sql_exists.asp
-app.get("/bronze_gold", (req, res) => {
-    const sql = `SELECT DISTINCT ag1.athlete_id, a.name
-      FROM athlete_game ag1
-      JOIN athletes a ON ag1.athlete_id = a.athlete_id
-      JOIN games g1 ON ag1.game_id = g1.game_id
-      LEFT JOIN athlete_event_medal aem1 ON ag1.athlete_id = aem1.athlete_id
-      WHERE g1.city = 'London' AND g1.year = 2012 AND aem1.medal = "Bronze"
-      AND EXISTS (
-          SELECT 1
-          FROM athlete_game ag2
-          JOIN games g2 ON ag2.game_id = g2.game_id
-          JOIN athlete_event_medal aem2 ON ag2.athlete_id = aem2.athlete_id
-          WHERE ag1.athlete_id = ag2.athlete_id AND aem2.medal = "Gold"
-          AND g2.city = 'Rio de Janeiro' AND g2.year = 2016
-  )`;
-    db.query(sql, function (err, result) {
-      if (err) throw err;
-      res.render("bronze_gold", {
-        data: result,
-      });
+
+app.get("/goldies", (req, res) => {
+  const sql = `WITH london_athletes AS (
+  SELECT DISTINCT a.name, ag.athlete_id, ag.game_id
+  FROM athlete_game ag
+  INNER JOIN athletes a ON ag.athlete_id = a.athlete_id
+  INNER JOIN athlete_event_medal aem ON ag.athlete_id = aem.athlete_id
+  WHERE ag.game_id = (
+    SELECT game_id
+    FROM games
+    WHERE year = 2012 AND city = 'London'
+  )
+  AND aem.medal = 'Gold'
+)
+
+SELECT la.athlete_id, la.name
+FROM london_athletes la
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM athlete_game ag2
+  INNER JOIN athlete_event_medal aem2
+  ON ag2.athlete_id = aem2.athlete_id
+  WHERE ag2.athlete_id = la.athlete_id AND ag2.game_id = la.game_id
+  AND aem2.medal IN ('No medal')
+)
+AND EXISTS (
+  SELECT 1
+  FROM athlete_game ag3
+  INNER JOIN athlete_event_medal aem3
+  ON ag3.athlete_id = aem3.athlete_id
+  WHERE ag3.athlete_id = la.athlete_id
+  AND aem3.medal IN ('Gold')
+  AND ag3.game_id = (
+    SELECT game_id
+    FROM games
+    WHERE year = 2016 AND city = 'Rio de Janeiro'
+  )
+)`;
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render("goldies", {
+      data: result,
     });
   });
+});
 
 // Question Ten: Was there any NOCs that participated in the 2010 Vancouver games but not the 2014 Sochi games?
 // Reference for using Except clause: https://www.tutorialspoint.com/sql/sql-except-clause.html
+// However, Coursera labs did not support Except clause, hence changed it to NOT IN.
 app.get("/vancouver_sochi", (req, res) => {
-  const sql = `(SELECT n.noc_id, n.noc, n.team
-    FROM noc n
-    LEFT JOIN athlete_game ag
-    ON n.noc_id = ag.noc_id
-    WHERE ag.game_id = (SELECT game_id FROM games WHERE year = '2014'))
-    EXCEPT
-    (SELECT n.noc_id, n.noc, n.team
-    FROM noc n
-    LEFT JOIN athlete_game ag
-    ON n.noc_id = ag.noc_id
-    WHERE ag.game_id = (SELECT game_id FROM games WHERE year = '2010'))`;
+  const sql = `SELECT n1.noc_id, n1.noc, n1.team
+FROM noc n1
+LEFT JOIN athlete_game ag1
+  ON n1.noc_id = ag1.noc_id
+WHERE ag1.game_id = (SELECT game_id FROM games WHERE year = '2014')
+  AND n1.noc_id NOT IN (
+    SELECT n2.noc_id
+    FROM noc n2
+    LEFT JOIN athlete_game ag2
+      ON n2.noc_id = ag2.noc_id
+    WHERE ag2.game_id = (SELECT game_id FROM games WHERE year = '2010')
+  );
+`;
   db.query(sql, function (err, result) {
     if (err) throw err;
     res.render("vancouver_sochi", {
